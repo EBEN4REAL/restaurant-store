541564ddae26e595df24401d16eb71d9855a132c
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2021-05-12 14:52:27 +0100

    .

--- src/components/RecipeForm/RecipeForm.vue
+++ src/components/RecipeForm/RecipeForm.vue
@@ -3,17 +3,17 @@
     <p>Introduce below the quantity for each ingredient you have and we will calculate how many Pasta Carbonara meals you can cook!</p>
     <form class="recipe-form__form">
       <label for="recipe-eggs">Eggs</label>
-      <input class="recipe-form__form-input" type="number" name="eggs" id="recipe-eggs" value="0" />
+      <input class="recipe-form__form-input" type="number" name="eggs" id="recipe-eggs" value="0" v-model="eggs" />
       <label for="recipe-pasta">Pasta</label>
-      <input class="recipe-form__form-input" type="number" name="pasta" id="recipe-pasta" value="0" />
+      <input class="recipe-form__form-input" type="number" name="pasta" id="recipe-pasta" value="0" v-model="pasta" />
       <label for="recipe-butter">Butter</label>
-      <input class="recipe-form__form-input" type="number" name="butter" id="recipe-butter" value="0" />
+      <input class="recipe-form__form-input" type="number" name="butter" id="recipe-butter" value="0" v-model="butter" />
       <label for="recipe-milk">Milk</label>
-      <input class="recipe-form__form-input" type="number" name="milk" id="recipe-milk" value="0" />
+      <input class="recipe-form__form-input" type="number" name="milk" id="recipe-milk" value="0" v-model="milk" />
       <label for="recipe-oil">Oil</label>
-      <input class="recipe-form__form-input" type="number" name="oil" id="recipe-oil" value="0" />
+      <input class="recipe-form__form-input" type="number" name="oil" id="recipe-oil" value="0" v-model="oil" />
       <label for="recipe-bacon">Bacon</label>
-      <input class="recipe-form__form-input" type="number" name="bacon" id="recipe-bacon" value="0" />
+      <input class="recipe-form__form-input" type="number" name="bacon" id="recipe-bacon" value="0" v-model="bacon" />
       <button type="submit" @click="calculate">Calculate</button>
     </form>
   </div>
@@ -26,7 +26,13 @@ export default {
   name: 'RecipeForm',
   data () {
     return {
-      meals: 0
+      meals: 0,
+      eggs: 0,
+      pasta: 0,
+      butter: 0,
+      bacon: 0,
+      milk: 0,
+      oil: 0
     };
   },
   props: {
@@ -38,7 +44,43 @@ export default {
   methods: {
     calculate (event) {
       event.preventDefault();
+      if(!this.eggs || !this.pasta || !this.butter || !this.milk || !this.bacon || !this.oil) {
+        this.$toast("Some fields are missing ", {
+            type: "error",
+            timeout: 3000
+        });
+        return
+      }
+      let ingredients = Object.assign({}, this.ingredients)
+      let ingredientsArr = []
+      for(let ingredient in ingredients) {
+        let quantity
+        quantity = ingredient === 'pasta' 
+          ? Math.floor( this.pasta / ingredients['pasta'] ) 
+          : ingredient === 'eggs' 
+          ? Math.floor( this.eggs / ingredients['eggs'] ) 
+          : ingredient === 'bacon'
+          ? Math.floor( this.bacon / ingredients['bacon'] )
+          : ingredient === 'oil'
+          ? Math.floor( this.oil / ingredients['oil'] )
+          : ingredient === 'milk'
+          ? Math.floor( this.milk / ingredients['milk'] )
+          : ingredient === 'butter'
+          ? Math.floor( this.butter / ingredients['butter'] )
+          : null
+        
+        ingredientsArr.push(quantity)
+      }
+
+      console.log(ingredientsArr)
+      this.meals = Math.min(...ingredientsArr)
+      this.$toast(`${this.meals} Meals`, {
+            type: "success",
+            timeout: 3000
+        });
     }
   }
 }
+
 </script>
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Toast from "vue-toastification";
+import "vue-toastification/dist/index.css";
+
 
 Vue.config.productionTip = false
+Vue.use(Toast);
+
 
 new Vue({
   router,
