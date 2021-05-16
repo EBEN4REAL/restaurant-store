bf53a82e6edfe446be033c1d3a94c780c9a0d064
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2021-05-16 22:11:05 +0100

    .

--- tests/unit/app.spec.js
+++ tests/unit/app.spec.js
@@ -1,11 +1,17 @@
 
-
 import { mount } from '@vue/test-utils'
 import App from '@/App.vue'
+import Meals from '@/views/Meals.vue'
 
 describe('App component', () => {
   it('renders the app component', () => {
     const wrapper = mount(App)
     expect(wrapper.contains('#app')).toBe(true)
+  })
+})
+describe('Meal  component', () => {
+  it('renders the meal component', () => {
+    const wrapper = mount(Meals)
+    expect(wrapper.contains('.page__carbonara')).toBe(true)
   })
 })
