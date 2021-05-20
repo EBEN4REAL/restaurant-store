cf3302afa370b325fe98539811435ed18564efe9
Eben4real <ebenezer.igbinoba@stu.cu.edu.ng>
2021-05-20 22:31:09 +0100

    Fetch Timeout requests

--- src/components/Store/Store.vue
+++ src/components/Store/Store.vue
@@ -42,11 +42,36 @@ export default {
     }
   },
   mounted(){
-    fetch('https://api.jokes.one/jod?category=knock-knock')
-      .then(response => response.json())
-      .then(res => {
+    this.fetchJokes()
+  },
+  methods: {
+    async  fetchJokes() {
+      try {
+        const response = await this.fetchWithTimeout('https://api.jokes.one/jod?category=knock-knock', {
+          timeout: 6000
+        });
+        const res = await response.json();
         this.joke = res.contents.jokes[0].joke.text
+      } catch (error) {
+        throw new Error(error)
+      }
+    },
+
+    async fetchWithTimeout(resource, options) {
+      const { timeout = 8000 } = options;
+      
+      const controller = new AbortController();
+      const id = setTimeout(() => controller.abort(), timeout);
+
+      const response = await fetch(resource, {
+        ...options,
+        signal: controller.signal  
       });
+      clearTimeout(id);
+
+      return response;
     }
+    
+  }
 }
 </script>
